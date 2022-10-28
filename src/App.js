e967fc7dc8736d6a8c5ab50e48d17f284d48c86d
Fayofunmi <akingbadeenoch125@gmail.com>
2022-10-28 15:41:21 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { RiMoreLine } from "react-icons/ri";
+import { RiShareForwardLine } from "react-icons/ri";
+import { BsGithub } from "react-icons/bs";
+import AVTR1 from "./image/avatar1.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <br></br>
+      <br></br>
+      <section className="profile">
+        <div className="share">
+          <RiShareForwardLine />
+        </div>
+        <div className="more">
+          <RiMoreLine />
+        </div>
+
+        <div id="profile__img">
+          <img src={AVTR1} alt="Akingbade Fayofunmi" />
+        </div>
+
+        <p> Akingbade Fayofunmi </p>
+        <p id="slack"> fayotheVillain </p>
+      </section>
+      <section className="link">
+        <button id="twitter">
+          <a
+            href="https://twitter.com/thegreatkidfayo"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Twitter Link
+          </a>
+        </button>
+        <button id="btn__zuri">
+          <a
+            href="https://training.zuri.team/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Zuri Team
+          </a>
+        </button>
+        <button id="books">
+          <a href="http://books.zuri.team/" target="_blank" rel="noreferrer">
+            Zuri Books
+          </a>
+        </button>
+        <button id="book__python">
+          <a href="https://books.zuri.team/" target="_blank" rel="noreferrer">
+            Python Books
+          </a>
+        </button>
+        <button id="pitch">
+          <a
+            href="https://background.zuri.team/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Background Check for Coders
+          </a>
+        </button>
+        <button id="book__design">
+          <a
+            href="https://training.zuri.team/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Design Books
+          </a>
+        </button>
+      </section>
+    </>
   );
 }
 
